4031f4a7570b9c1c9a6e5b7a50b018ba8a67754c
Nídio Dolfini <nidiosdolfini@gmail.com>
2020-07-14 17:52:25 -0300

    Update Conta.java

--- Segunda Aula/Conta.java
+++ Segunda Aula/Conta.java
@@ -1,7 +1,27 @@
-class Conta {
+public class Conta {
     String titular;
     int numero;
     String agencia;
     double saldo;
-    boolean estadoCivil;
+    String dataDeAbertura;
+
+    void saca(double valor) {
+        this.saldo -= valor;
+    }
+
+    void deposita(double valor) {
+        this.saldo += valor;
+    }
+
+    double calculaRendimento() {
+        return this.saldo * 0.1;
+    }
+
+    public String recuperaDadosParaImpressao() {
+        String dados = "Titular: "	+ this.titular;
+        dados += "\nNúmero:	" + this.numero;
+        dados += "\nAgencia: " + this.agencia;
+        dados += "\nSaldo: " + this.saldo;
+        return	dados;
+    }
 }
